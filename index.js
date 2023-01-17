0e271853f6f2926c4594083525f0050ff19ff125
charnoodle <chadavidcpa@gmail.com>
2023-01-17 19:39:38 +0800

    Facebook plugin

--- index.js
+++ index.js
@@ -39,3 +39,12 @@ window.addEventListener("scroll", reveal);
 
 // To check the scroll position on page load
 reveal();
+
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
